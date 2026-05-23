ecc71594317106f81cef9bf2563a825dd8c47ef8
github-action-benchmark <github@users.noreply.github.com>
2026-05-23 10:17:39 +0000

    add Benchmark (cargo) benchmark result for f234953ecffb7997661decac8401a1a2ebaa947f

---
+++ dev/bench/data.js
@@ -0,0 +1,86 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1779531459090,
+  "repoUrl": "https://github.com/isidornygren/ergoap",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "me@isidornygren.com",
+            "name": "Isidor Nygren",
+            "username": "isidornygren"
+          },
+          "committer": {
+            "email": "me@isidornygren.com",
+            "name": "Isidor Nygren",
+            "username": "isidornygren"
+          },
+          "distinct": true,
+          "id": "f234953ecffb7997661decac8401a1a2ebaa947f",
+          "message": "Auto push to github pages",
+          "timestamp": "2026-05-23T12:02:28+02:00",
+          "tree_id": "c750ad1b7b02c1e7b26c66bc397e3c2a8b7933ee",
+          "url": "https://github.com/isidornygren/ergoap/commit/f234953ecffb7997661decac8401a1a2ebaa947f"
+        },
+        "date": 1779531458803,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "spawn_action_provider",
+            "value": 337486,
+            "range": "± 9727",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sensor_update/short/1",
+            "value": 2795210,
+            "range": "± 207140",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sensor_update/long/1",
+            "value": 3290494,
+            "range": "± 226637",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sensor_update/short/10",
+            "value": 3027513,
+            "range": "± 96412",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sensor_update/long/10",
+            "value": 5952678,
+            "range": "± 136774",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sensor_update/short/50",
+            "value": 4948832,
+            "range": "± 460058",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sensor_update/long/50",
+            "value": 18088742,
+            "range": "± 514812",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sensor_update/short/100",
+            "value": 7258739,
+            "range": "± 509438",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sensor_update/long/100",
+            "value": 34248867,
+            "range": "± 794998",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
